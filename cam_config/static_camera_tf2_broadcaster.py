4afa889e54402be101bd29db9c026f6bd409f866
KarolineH <karoline@heiwolt.de>
2024-02-28 14:35:14 +0000

    First commit

---
+++ cam_config/static_camera_tf2_broadcaster.py
@@ -0,0 +1,91 @@
+import math
+import sys
+
+from geometry_msgs.msg import TransformStamped
+
+import numpy as np
+
+import rclpy
+from rclpy.node import Node
+
+from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
+
+
+# This function is a stripped down version of the code in
+# https://github.com/matthew-brett/transforms3d/blob/f185e866ecccb66c545559bc9f2e19cb5025e0ab/transforms3d/euler.py
+# Besides simplifying it, this version also inverts the order to return x,y,z,w, which is
+# the way that ROS prefers it.
+def quaternion_from_euler(ai, aj, ak):
+    ai /= 2.0
+    aj /= 2.0
+    ak /= 2.0
+    ci = math.cos(ai)
+    si = math.sin(ai)
+    cj = math.cos(aj)
+    sj = math.sin(aj)
+    ck = math.cos(ak)
+    sk = math.sin(ak)
+    cc = ci*ck
+    cs = ci*sk
+    sc = si*ck
+    ss = si*sk
+
+    q = np.empty((4, ))
+    q[0] = cj*sc - sj*cs
+    q[1] = cj*ss + sj*cc
+    q[2] = cj*cs - sj*sc
+    q[3] = cj*cc + sj*ss
+
+    return q
+
+
+class StaticFramePublisher(Node):
+    """
+    Broadcast transform from `tool_frame` to the mounted camera.
+    This needs to be calibrated or changed whenever the camera mount is adjusted.
+    The transform is only published once at startup, and is constant for all
+    time.
+    """
+
+    def __init__(self, transformation):
+        super().__init__('static_turtle_tf2_broadcaster')
+
+        self.tf_static_broadcaster = StaticTransformBroadcaster(self)
+
+        # Publish static transforms once at startup
+        self.make_transforms(transformation)
+
+    def make_transforms(self, transformation):
+        t = TransformStamped()
+
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'tool_frame'
+        t.child_frame_id = transformation[0]
+
+        t.transform.translation.x = float(transformation[1])
+        t.transform.translation.y = float(transformation[2])
+        t.transform.translation.z = float(transformation[3])
+        quat = quaternion_from_euler(
+            float(transformation[4]), float(transformation[5]), float(transformation[6]))
+        t.transform.rotation.x = quat[0]
+        t.transform.rotation.y = quat[1]
+        t.transform.rotation.z = quat[2]
+        t.transform.rotation.w = quat[3]
+
+        self.tf_static_broadcaster.sendTransform(t)
+
+
+def main():
+    args = ['cam_frame', 0, -0.045, 0.085, -math.pi/2, 0, 0] # child_frame_name x y z roll pitch yaw
+
+    rclpy.init()
+    node = StaticFramePublisher(args)
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'cam_config'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Karoline Heiwolt',
+    maintainer_email='contact@karoline.heiwolt.de',
+    description='Package containing configurations for a Canon EOS DSLR camera mounted onto a Kinova Gen3 robot arm',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'static_camera_tf2_broadcaster = cam_config.static_camera_tf2_broadcaster:main',
+        ],
+    },
+)
